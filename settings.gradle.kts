478dc79b6e7bb79c732b97c81ec90f36819b523c
li.qi <15868175516@163.com>
2024-06-08 18:50:53 +0800

    build: declare repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,22 @@
+pluginManagement {
+    repositories.gradlePluginPortal()
+}
+
 rootProject.name = "microsphere-devops"
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/releases/")
+        }
+        maven {
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        }
+        mavenCentral()
+    }
+}
+
+include(":microsphere-devops-api")
